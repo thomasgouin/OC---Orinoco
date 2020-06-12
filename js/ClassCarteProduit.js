7c28d5237d7354846eee271adced2016297a719f
Thomas Gouin <thomas.gouin@gmail.com>
2020-06-12 09:51:00 +0200

    modification de l'option lentilles en page produit

--- js/ClassCarteProduit.js
+++ js/ClassCarteProduit.js
@@ -21,7 +21,7 @@ class Carte {
         const wrapper = document.getElementById('wrapper');
         const carte = document.createElement('article');carte.classList.add('carte');carte.setAttribute("id", this._id);
         const carteImage = document.createElement('img');carteImage.classList.add('carte__image');carteImage.setAttribute("src", this.imageUrl);
-        const cartePrix = document.createElement('div');cartePrix.classList.add('carte__prix');cartePrix.textContent = Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(this.price/100)
+        const cartePrix = document.createElement('div');cartePrix.classList.add('carte__prix');cartePrix.textContent = this.displayPrice('fr-FR', 'EUR');
         const carteDescription = document.createElement('div');carteDescription.classList.add('carte__description');
         const carteDescriptionHover = document.createElement('div');carteDescriptionHover.classList.add('carte__description-hover');carteDescriptionHover.textContent = this.description;
         const carteTitre = document.createElement('h2'); carteTitre.classList.add('carte__description__titre');carteTitre.textContent = this.name;
@@ -41,39 +41,15 @@ class Carte {
         
         // Création de l'URL avec les différents paramètres
         const urlElement = new URL(" http://127.0.0.1:5500/produit.html");
-            urlElement.searchParams.set("name", this.name);
             urlElement.searchParams.set("id", this._id);
-            urlElement.searchParams.set("price", this.price);
-            urlElement.searchParams.set("description", this.description);
-            urlElement.searchParams.set("image", this.imageUrl);
-            urlElement.searchParams.set("lenses", this.lenses);   
+            
             
         carte.addEventListener('click', function(){
             window.location.href = urlElement;
         });
-    };   
+    }
+    
+    displayPrice(lang, money){
+        return Intl.NumberFormat(lang, { style: 'currency', currency: money }).format(this.price/100)
+    }
 }
-
-
-/*
-const blocCarteAppareil = data.map(appareil =>{
-    let afficheCart = `<a href="./produit.html?${appareil._id}">
-                            <article class="carte">
-                                <img class="carte__image" src="${appareil.imageUrl}" alt=""></img>
-                                <div class="carte__prix">${Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(appareil.price/100)}</div>
-                                <div class="carte__description">
-                                    <h2 class="carte__description__titre">${appareil.name}</h2>
-                                    <ul class="carte__description__option">`
-                                        appareil.lenses.forEach(lense => {
-                                            afficheCart +=`<li>${lense}</li>`
-                                        })
-                                    afficheCart+=`</ul>
-                                </div>
-                                <div class="carte__description-hover">${appareil.description}</div>
-                            </article>
-                        </a>`;
-    return afficheCart;
-})
-        
-document.getElementById('wrapper').innerHTML = blocCarteAppareil;
-*/
--- js/produit.js
+++ js/produit.js
@@ -9,34 +9,31 @@
 
 const myUrl = new URL(window.location.href);
 
-let appareil = {
-    name : myUrl.searchParams.get("name"),
-    _id : myUrl.searchParams.get("id"),
-    description : myUrl.searchParams.get("description"),
-    image : myUrl.searchParams.get("image"),
-    lenses : myUrl.searchParams.get("lenses"),
-    price :  myUrl.searchParams.get("price"),
-}
+const fetchApi = async function () {
+    const response = await fetch ('http://localhost:3000/api/cameras/'+ myUrl.searchParams.get("id"))
+    const jsonData = await response.json();
 
-document.getElementById('name').textContent = appareil.name;
-document.getElementById('description').textContent = appareil.description;
-document.getElementById('price').textContent = Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(appareil.price/100);
-document.getElementById('image').setAttribute("src", appareil.image);
+    let appareil = jsonData;
 
-// Gestion de la liste déroulante pour les options de lentilles 
- // /!\ NON FONCTIONNEL
-const select = document.createElement('select');select.classList.add('lentille');select.setAttribute('id', 'lentille');
-const divSelecteur = document.getElementById('selecteur'); 
-const option = document.createElement('option');option.textContent = appareil.lenses;
+    document.getElementById('name').textContent = appareil.name;
+    document.getElementById('description').textContent = appareil.description;
+    document.getElementById('price').textContent = Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(appareil.price/100);
+    document.getElementById('image').setAttribute("src", appareil.imageUrl)
 
-select.appendChild(option);
-divSelecteur.appendChild(select);
+    let select = `<select><option value="">Choisissez votre lentille</option>`;
 
+    appareil.lenses.forEach(element => {
+        select += `<option>${element}</option>`
+    })
+        select += `</select>`;
+    document.getElementById('selecteur').innerHTML = select; 
 
-// Au clic sur le bouton, envoie les information dans le localStorage
-const button = document.getElementById('button');
-button.onclick = function () {
-    let appareilJSONString = JSON.stringify(appareil);
-    localStorage.setItem("appareil", appareilJSONString);
-}
+    const button = document.getElementById('button');
+   // const panierExistant = JSON.parse(localStorage.getItem(""));
+
+    button.onclick = function () {
+        localStorage.setItem(appareil.name, appareilJSONString);
+    } 
+} 
+fetchApi();
 
