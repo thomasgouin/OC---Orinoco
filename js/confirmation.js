5a86d01423273cb937461ae013af43172ee6eff0
Thomas Gouin <thomas.gouin@gmail.com>
2020-06-22 16:26:03 +0200

    modification favicon et fonction clear card en page confirmation

--- js/confirmation.js
+++ js/confirmation.js
@@ -9,11 +9,12 @@ let lastName = myUrl.searchParams.get("lastName");
 let id = myUrl.searchParams.get("id");
 let totalPrice = myUrl.searchParams.get("totalPrice");
 
-// ON ajoute le texte de remerciement et les différentes valeurs de la commande grâce au inner HTML. 
+// On ajoute le texte de remerciement et les différentes valeurs de la commande grâce au inner HTML. 
 blocConfirmation.innerHTML = `<p class="confirmation__titre">Merci pour votre confiance,</p>
 <p class="confirmation__paragraphe">Cher <span id="confirmPrenom">${firstName}</span> <span id="confirmNom">${lastName}</span>, nous vous confirmons que votre commande de <span id="confirmPTotal">${totalPrice}</span>, est bien enregistrée. Vous recevrez un accusé de réception à l'adresse mail indiquée.</p>
 <P class="confirmation__paragraphe">Pour suivre son acheminement, renseignez votre n° de commande : <span id="numCommande">${id}</span> lorsque vous contactez notre service après vente.</P>`
 
+//On crée une fonction pour vider le panier. 
 function clearCart(){
     let cartToClear = localStorage.getItem('panier');
     if (cartToClear != null) {
